672d73c73dcb0f432fcdc421cdbbff98d774afc6
Ben Campbell <ben.campbell@unh.edu>
2023-06-04 21:52:56 -0400

    task info finished

--- src/native/task.py
+++ src/native/task.py
@@ -18,6 +18,7 @@ class TaskBar(QWidget):
         self.color = QColor(245, 245, 245)
         self._padding = padding
         self._paint_rect = QRect(QRect(self._padding, self._padding, self.size().width() - 2*self._padding, self.size().height() - 2*self._padding))
+        self._vertical_margin = 0
         self.setFixedHeight(60)
 
     def get_paint_rect(self):
@@ -41,6 +42,7 @@ class TaskBar(QWidget):
         
         # Background
         brush.setColor(self.color)
+        self._paint_rect.setHeight(self._paint_rect.height() + self._vertical_margin)
         path.addRoundedRect(self._paint_rect, 15, 15)
         painter.fillPath(path, brush)
 
@@ -50,9 +52,44 @@ class TaskBar(QWidget):
         brush.setColor(self.color)
         _progress_rect = QRect(self._paint_rect)
         _progress_rect.setWidth(self._paint_rect.width() * self.value)
+        _progress_rect.setHeight(_progress_rect.height() + self._vertical_margin)
         path.addRoundedRect(_progress_rect, 15, 15)
         painter.fillPath(path, brush)
 
+class TaskInfo(QWidget):
+    def __init__(self):
+        super().__init__(minimumHeight=0, maximumHeight=0)
+        
+        self.background = QColor(250, 250, 250)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        
+        self._content = QScrollArea()
+        self._content.setStyleSheet('background: transparent')
+        self._layout = QVBoxLayout(self._content)
+        self._layout.setContentsMargins(20,5,20,10)
+    
+        self._layout.addWidget(QLabel("Total Images: 10"))
+        self._layout.addWidget(QLabel("Time remaining: 10h 15m 32s"))    
+        for i in range(10):
+            self._layout.addWidget(QCheckBox(f"Image {i}: Size: 50nm, Offset: (-317.82 , 401.20) nm, Bias: {50*i + 25} V, Setpoint: 120pA", checked=True))
+            
+        self.setLayout(QGridLayout())
+        self.layout().addWidget(self._content)
+        self.layout().setContentsMargins(0,0,0,0)
+            
+    def paintEvent(self, e):
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
+        brush = QtGui.QBrush()
+        brush.setStyle(Qt.SolidPattern)
+        path = QtGui.QPainterPath()
+        
+        # Background
+        brush.setColor(self.background)
+        vertical_margin = 15
+        background_rect = QRect(self.rect().left(), self.rect().top() - vertical_margin, self.rect().width(), self.rect().height() + vertical_margin)
+        path.addRoundedRect(background_rect, 15, 15)
+        painter.fillPath(path, brush)
 
 class Task(QWidget):
     Status = Enum('Status', ['Ready', 'Working', 'Finished'])
@@ -64,12 +101,12 @@ class Task(QWidget):
         self.setMaximumHeight(60)
 
         self._content = QWidget()
-        self._task_bar = TaskBar(value=0.0)
+        self._task_bar = TaskBar(value=0.5)
         self._icon = QPushButton(icon=fa.icon('fa5.circle', color='black'))
         self._icon.setFlat(True)
         self._name = QLineEdit(name)
         self._name.setStyleSheet('QLineEdit { background: transparent; color: black; border: 0px; }')
-        self._options_btn = QPushButton(icon=fa.icon('fa5s.caret-left', color='black', scale_factor=2.0))
+        self._options_btn = QPushButton(icon=fa.icon('fa5s.ellipsis-v', color='black', scale_factor=1.0))
         self._options_btn.setFlat(True)        
         self._content.setFixedHeight(self._task_bar.rect().height())
 
@@ -83,17 +120,7 @@ class Task(QWidget):
         self._content_layout.setColumnStretch(2, 1)
         self._content_layout.setContentsMargins(0,0,0,0)
         
-        self._info = QScrollArea(minimumHeight=0, maximumHeight=0)
-        self._info.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        
-        self._info_layout = QVBoxLayout(self._info)
-        self._info_layout.setContentsMargins(20,0,20,10)
-        
-        self._info_layout.addWidget(QLabel("Total Images: 10"))
-        self._info_layout.addWidget(QLabel("Time remaining: 10h 15m 32s"))
-        
-        for i in range(10):
-            self._info_layout.addWidget(QCheckBox(f"Image {i}: Size: 50nm, Offset: (-317.82 , 401.20) nm, Bias: {50*i + 25} V, Setpoint: 120pA", checked=True))
+        self._info = TaskInfo()
 
         self._layout = QGridLayout(self)
         self._layout.addWidget(self._info, 1, 0)
@@ -107,8 +134,8 @@ class Task(QWidget):
 
         self.installEventFilter(self)
 
-        self._task_bar_anim = QPropertyAnimation(self._task_bar, b"paint_rect")
-        self._task_bar_anim.setDuration(100)
+        self._task_bar_hover_anim = QPropertyAnimation(self._task_bar, b"paint_rect")
+        self._task_bar_hover_anim.setDuration(100)
         
         self._info_anim = QParallelAnimationGroup(self)
         self.setInfoAnimation()
@@ -120,7 +147,7 @@ class Task(QWidget):
         self._info_anim.addAnimation(QPropertyAnimation(self._info, b"maximumHeight"))
         
         collapsed_height = self.sizeHint().height() - self._info.maximumHeight()
-        content_height = self._info_layout.sizeHint().height()
+        content_height = self._info._layout.sizeHint().height()
 
         anims = [self._info_anim.animationAt(i) for i in range(self._info_anim.animationCount())]
         for anim in anims[:-1]:
@@ -135,13 +162,13 @@ class Task(QWidget):
         
         if ev.type() == QtCore.QEvent.Enter:
             if not self._selected:
-                self._task_bar_anim.setEndValue(self._task_bar.rect())
-                self._task_bar_anim.start()
+                self._task_bar_hover_anim.setEndValue(self._task_bar.rect())
+                self._task_bar_hover_anim.start()
 
         if ev.type() == QtCore.QEvent.Leave:
             if not self._selected:
-                self._task_bar_anim.setEndValue(QRect(self._task_bar._padding, self._task_bar._padding, self._content.size().width() - 2*self._task_bar._padding, self._content.size().height() - 2*self._task_bar._padding))
-                self._task_bar_anim.start()
+                self._task_bar_hover_anim.setEndValue(QRect(self._task_bar._padding, self._task_bar._padding, self._content.size().width() - 2*self._task_bar._padding, self._content.size().height() - 2*self._task_bar._padding))
+                self._task_bar_hover_anim.start()
             
         if ev.type() == QtCore.QEvent.MouseButtonPress:
             self._lastpos = ev.pos()
@@ -154,9 +181,11 @@ class Task(QWidget):
                 if not self._selected:
                     self._selected = True
                     self._info_anim.setDirection(QtCore.QAbstractAnimation.Forward)
+                    self._task_bar._vertical_margin = 10
                 else:
                     self._selected = False
                     self._info_anim.setDirection(QtCore.QAbstractAnimation.Backward)
+                    self._task_bar._vertical_margin = 0
                     
                 self._info_anim.start()
 
